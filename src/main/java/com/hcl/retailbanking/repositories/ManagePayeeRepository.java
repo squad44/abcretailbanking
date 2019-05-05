13df08318249d76832857314b7294c1dc1d58b52
nithin2889 <nithin2889@gmail.com>
2019-05-05 02:59:58 -0700

    adding manage payee

---
+++ src/main/java/com/hcl/retailbanking/repositories/ManagePayeeRepository.java
@@ -0,0 +1,9 @@
+package com.hcl.retailbanking.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hcl.retailbanking.entities.ManagePayee;
+
+public interface ManagePayeeRepository extends JpaRepository<ManagePayee, Long> {
+
+}
