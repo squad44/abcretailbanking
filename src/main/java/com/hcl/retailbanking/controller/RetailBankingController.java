41849db3bd24eccb3863747428af4bb70a2661a0
nithin2889 <nithin2889@gmail.com>
2019-05-05 00:29:18 -0700

    adding files

--- src/main/java/com/hcl/retailbanking/controller/RetailBankingController.java
+++ src/main/java/com/hcl/retailbanking/controller/RetailBankingController.java
@@ -1,8 +1,30 @@
 package com.hcl.retailbanking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.retailbanking.entities.CustomerCreationEntity;
+import com.hcl.retailbanking.service.RetailBankingService;
 
 @RestController
 public class RetailBankingController {
 
+	@Autowired
+	RetailBankingService retailbankService;
+	
+	@PostMapping("/customerLogin")
+	public Response customerLogin(@RequestParam("id") int id) {
+		if (retailbankService.validateUser(id)) {
+			return new Response("Welcome", Boolean.TRUE);
+		}
+		return new Response("Please check the user id", Boolean.FALSE);
+	}
+
+	@PostMapping("/accountSummary")
+	public CustomerCreationEntity getAccountSummary(@RequestParam("id") Long id) {
+		return retailbankService.fetchAccountSummary(id);
+	}
+	
 }
--- src/main/java/com/hcl/retailbanking/service/RetailBankingService.java
+++ src/main/java/com/hcl/retailbanking/service/RetailBankingService.java
@@ -1,5 +1,14 @@
 package com.hcl.retailbanking.service;
 
+import org.springframework.stereotype.Service;
+
+import com.hcl.retailbanking.entities.CustomerCreationEntity;
+
+@Service
 public interface RetailBankingService {
 
+	boolean validateUser(long id);
+
+	CustomerCreationEntity fetchAccountSummary(long id);
+	
 }
