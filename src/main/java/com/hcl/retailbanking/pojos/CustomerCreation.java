d24da1aac090bf3bc7154f9f4d1d2080cda22504
akashaman29 <36605898+akashaman29@users.noreply.github.com>
2019-05-05 13:42:34 +0530

    files added for customercreation api

--- src/main/java/com/hcl/retailbanking/pojos/CustomerCreation.java
+++ src/main/java/com/hcl/retailbanking/pojos/CustomerCreation.java
@@ -1,5 +1,50 @@
 package com.hcl.retailbanking.pojos;
 
+import java.util.Date;
+
 public class CustomerCreation {
+	
+	private String customer_name;
+	private Date customer_dob;
+	private long customer_phone_number;
+	private String account_type;
+	private double balance;
+	public String getCustomer_name() {
+		return customer_name;
+	}
+	public void setCustomer_name(String customer_name) {
+		this.customer_name = customer_name;
+	}
+	public Date getCustomer_dob() {
+		return customer_dob;
+	}
+	public void setCustomer_dob(Date customer_dob) {
+		this.customer_dob = customer_dob;
+	}
+	public long getCustomer_phone_number() {
+		return customer_phone_number;
+	}
+	public void setCustomer_phone_number(long customer_phone_number) {
+		this.customer_phone_number = customer_phone_number;
+	}
+	public String getAccount_type() {
+		return account_type;
+	}
+	public void setAccount_type(String account_type) {
+		this.account_type = account_type;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	@Override
+	public String toString() {
+		return "CustomerCreation [customer_name=" + customer_name + ", customer_dob=" + customer_dob
+				+ ", customer_phone_number=" + customer_phone_number + ", account_type=" + account_type + ", balance="
+				+ balance + "]";
+	}
 
 }
--- src/main/java/com/hcl/retailbanking/repositories/CustomerCreationRepository.java
+++ src/main/java/com/hcl/retailbanking/repositories/CustomerCreationRepository.java
@@ -2,6 +2,8 @@ package com.hcl.retailbanking.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerCreationRepository extends JpaRepository<T, ID> {
+import com.hcl.retailbanking.pojos.CustomerCreation;
+
+public interface CustomerCreationRepository extends JpaRepository<CustomerCreation, Long> {
 
 }
