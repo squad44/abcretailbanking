9cc549b1a1c4d1302f8067a674f98e7df390dcf5
hariharamohanty@gmail.com <hariharamohanty@gmail.com>
2019-05-05 03:34:48 -0700

    add autowired in ManagePayeeController.java

--- src/main/java/com/hcl/retailbanking/controller/ManagePayeeController.java
+++ src/main/java/com/hcl/retailbanking/controller/ManagePayeeController.java
@@ -2,12 +2,18 @@ package com.hcl.retailbanking.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hcl.retailbanking.service.ManagePayeeService;
+
 @RestController
 public class ManagePayeeController {
+	
+	@Autowired 
+	private ManagePayeeService managePayeeService;
 	
 	@GetMapping("/viewPayee/{accountId}")
 	public List<ManagePayee> viewPayee(@PathVariable Long accountId){
